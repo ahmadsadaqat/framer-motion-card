5344d0f8ef3b6ffb390e474693a3d6b60042898f
Ahmad Sadaqat <ahmadsadaqat6161@gmail.com>
2023-05-24 13:06:47 +0500

    framer motion added to todos

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,141 @@
-import Image from 'next/image'
+"use client";
+
+import { useState } from "react";
+import { motion } from "framer-motion";
 
 export default function Home() {
+  const [step, setStep] = useState(1);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex min-h-screen items-start bg-gradient-to-br from-slate-700 ">
+      <div className="mx-auto w-full max-w-md rounded-2xl bg-white">
+        <div className="flex justify-between rounded p-8">
+          {/* steps are here */}
+          <Step step={1} currentStep={step} />
+          <Step step={2} currentStep={step} />
+          <Step step={3} currentStep={step} />
+          <Step step={4} currentStep={step} />
+        </div>
+        <div className="px-8 pb-8">
+          <div>
+            <div className="mt-2 h-6 w-40 rounded bg-slate-100" />
+            <div className="mt-4 space-y-2">
+              <div className="h-4 w-5/6 rounded bg-slate-100" />
+              <div className="h-4 rounded bg-slate-100" />
+              <div className="h-4 w-4/6 rounded bg-slate-100" />
+            </div>
+          </div>
+
+          <div className="mt-10 flex justify-between">
+            <button
+              onClick={() => setStep(step < 2 ? step : step - 1)}
+              className="rounded px-2 py-1 text-slate-400 hover:text-slate-700"
+            >
+              back
+            </button>
+            <button
+              onClick={() => setStep(step > 4 ? step : step + 1)}
+              className={`${
+                step > 4 ? "pointer-events-none opacity-50" : ""
+              } bg flex items-center justify-center rounded-full bg-blue-500 py-1.5 px-4`}
+            >
+              Continue
+            </button>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  )
+  );
+}
+
+function Step({ step, currentStep }) {
+  let status =
+    currentStep === step
+      ? "active"
+      : currentStep < step
+      ? "inactive"
+      : "complete";
+
+  return (
+    <motion.div animate={status} className="relative">
+      <motion.div
+        variants={{
+          active: {
+            scale: 1,
+            // opacity: 1,
+            transition: {
+              delay: 0,
+              duration: 0.3,
+            },
+          },
+          complete: {
+            scale: 1.25,
+            // opacity: 0,
+          },
+        }}
+        transition={{
+          duration: 0.6,
+          delay: 0.3,
+          type: "tween",
+          ease: "circOut",
+        }}
+        className=" bg-blue-200 absolute inset-0 rounded-full"
+      ></motion.div>
+      <motion.div
+        initial={false}
+        variants={{
+          inactive: {
+            backgroundColor: "#fff",
+            borderColor: "rgb(148 163 184)",
+            color: "rgb(148 163 184)",
+          },
+          active: {
+            backgroundColor: "#fff",
+            borderColor: "rgb(59 130 246)",
+            color: "rgb(59 130 246)",
+          },
+          complete: {
+            backgroundColor: "rgb(59 130 246)",
+            borderColor: "rgb(59 130 246)",
+            color: "#fff",
+          },
+        }}
+        transition={{ duration: 0.2 }}
+        className={`relative flex h-10 w-10 items-center justify-center rounded-full border-2 font-semibold`}
+      >
+        <div className="flex items-center justify-center">
+          {status === "complete" ? (
+            <CheckIcon className="h-6 w-6 text-white" />
+          ) : (
+            <span>{step}</span>
+          )}
+        </div>
+      </motion.div>
+    </motion.div>
+  );
+}
+
+function CheckIcon(props) {
+  return (
+    <svg
+      {...props}
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      strokeWidth={3}
+    >
+      <motion.path
+        initial={{ pathLength: 0 }}
+        animate={{ pathLength: 1 }}
+        transition={{
+          delay: 0.2,
+          type: "tween",
+          ease: "easeOut",
+          duration: 0.3,
+        }}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M5 13l4 4L19 7"
+      />
+    </svg>
+  );
 }
